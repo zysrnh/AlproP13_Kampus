17b94485d1d6afde7359c5ad36bf85fb2e5ebe0d
Naooo <naooolaf@gmail.com>
2026-01-07 11:50:23 +0700

    Test

---
+++ Prk-01.py
@@ -0,0 +1,5 @@
+aX=[2,-91,-2,15,0,3,-21,7,10];
+N=len(aX);
+for i in range(N):
+    if i%2!=0:
+        print(aX[i]);
---
+++ Prk-02.py
@@ -0,0 +1,5 @@
+zT=[-2,-91,-2,5,0,3,-21,7,7]
+N=len(zT)
+for i in range(N):
+    if i%2==0:  
+        print(zT[i])
